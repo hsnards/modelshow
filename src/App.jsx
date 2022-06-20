74bf4d356d136a91eda20538369f9ed7f4f4b04f
hsnards <1214hassan1214@gmail.com>
2022-06-20 23:01:46 +0430

    create model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import ModelsComponent from './components/models'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,15 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
+       <ModelsComponent />
         <p>
           <a
             className="App-link"
---
+++ src/components/models/index.jsx
@@ -0,0 +1,10 @@
+import Styles from "./model.module.scss"
+const ModelsComponent = () => {
+    return ( 
+        <div className={Styles.root}>
+            Models
+        </div>
+     );
+}
+ 
+export default ModelsComponent
