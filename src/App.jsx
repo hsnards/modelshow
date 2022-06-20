fa47a8deb6e0674f7ebb595e482beee991be4cd3
hsnards <1214hassan1214@gmail.com>
2022-06-21 00:33:55 +0430

    test slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 import ModelsComponent from './components/models'
-
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import "swiper/css/effect-fade";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <ModelsComponent />
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <div style={{overflow:"hidden"}}>
+      <ModelsComponent />
+     </div>
   )
 }
 
--- src/components/models/index.jsx
+++ src/components/models/index.jsx
@@ -1,9 +1,35 @@
 import Styles from "./model.module.scss"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, Pagination ,EffectFade } from "swiper";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "swiper/css/effect-fade";
+import 'swiper/css/scrollbar';
 const ModelsComponent = () => {
     return ( 
-        <div className={Styles.root}>
-            Models
-        </div>
+         <div  className={Styles.root}>
+        <Swiper
+          direction={"vertical"}
+                slidesPerView={1}
+                effect={"fade"}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Mousewheel, Pagination ,EffectFade]}
+         className="mySwiper"
+        >
+                <SwiperSlide className={Styles.swiperSlider}>
+                    <img  src="/public/image.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className={Styles.swiperSlider}>
+                    <img src="/public/278865405_1157208258359668_3959650466254873679_n.jpg" />
+                </SwiperSlide>
+          
+        </Swiper>
+      </div>
      );
 }
  
